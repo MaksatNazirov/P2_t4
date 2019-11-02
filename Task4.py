e34ebd84f804ceb346d5abf4ecb8a995faf56a5b
Maks <nazirov.maksat96@gmail.com>
2019-11-02 08:51:36 +0600

    P2_t4

---
+++ Task4.py
@@ -0,0 +1,11 @@
+# def clock_face():   
+
+#     num_hours1 = 6
+#     num_min1 = 1
+#     num_sec1 = 30
+#     num_hours2 = 6
+#     num_min2 = 2
+#     num_sec2 = 10
+
+#     print((((num_hours2 - num_hours1)*3600) + (num_min2 - num_min1)*60) + (num_sec2 - num_sec1))
+# clock_face()
